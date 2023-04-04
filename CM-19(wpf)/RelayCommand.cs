6c80a5f0a5ce2f4b49abb242645345e38ed27972
Maria_Arsen <maria_m_84@inbox.ru>
2023-04-04 15:57:12 +0300

    Добавьте файлы проекта.

---
+++ CM-19(wpf)/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CM_19_wpf_
+{
+    class RelayCommand : ICommand
+    {
+        public readonly Action<object> execute;
+        public readonly Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        
+        public RelayCommand(Action<object> Execute,Func<object,bool>CanExecute=null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+
+        public void Execute(object parameter) => execute(parameter);
+      
+    }
+}
